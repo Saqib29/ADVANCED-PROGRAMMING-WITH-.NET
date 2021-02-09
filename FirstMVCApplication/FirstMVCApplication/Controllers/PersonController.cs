2cea071ddea1abb40319397806068686caf2a73b
Saqib <saqib29111@gmail.com>
2021-02-09 15:48:21 +0600

    PersonController folowing the configuration

---
+++ FirstMVCApplication/FirstMVCApplication/Controllers/PersonController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVCApplication.Controllers
+{
+    public class PersonController : Controller
+    {
+        public string Index()
+        {
+            return "Hello, this is from person controller index method";
+        }
+    }
+}
