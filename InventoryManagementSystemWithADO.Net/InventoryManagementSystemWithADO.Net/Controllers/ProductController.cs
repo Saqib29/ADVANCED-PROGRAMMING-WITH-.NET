61049c73f2179a20a882714a337c9ae4088a04e0
Saqib <saqib29111@gmail.com>
2021-02-14 20:17:42 +0600

    show products, insert, update and delete complete for products

---
+++ InventoryManagementSystemWithADO.Net/InventoryManagementSystemWithADO.Net/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using InventoryManagementSystemWithADO.Net.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InventoryManagementSystemWithADO.Net.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductModel productModel = new ProductModel();
+        public ActionResult Index()
+        {
+            return View(productModel.GetProducts());
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            productModel.Insert(product);
+            return RedirectToAction("Index", "Product");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            return View(productModel.getProduct(id));
+        }
+        [HttpPost]
+        public ActionResult Edit(int id,Product product)
+        {
+            product.ProductId = id;
+            productModel.Update(product);
+            return RedirectToAction("Index", "Product");
+            //return Content(product.ProductName +"   "+product.ProductId);
+
+        }
+        public ActionResult Delete(int id)
+        {
+            productModel.Delete(id);
+            return RedirectToAction("Index", "Product");
+        }
+    }
+}
---
+++ InventoryManagementSystemWithADO.Net/InventoryManagementSystemWithADO.Net/Models/ProductModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace InventoryManagementSystemWithADO.Net.Models
+{
+    public class ProductModel
+    {
+        DataAccess dataAccess;
+        public ProductModel()
+        {
+            dataAccess = new DataAccess();
+        }
+        public List<Product> GetProducts()
+        {
+            string sql = "SELECT * FROM Products";
+            SqlDataReader reader =  dataAccess.GetData(sql);
+
+            List<Product> products = new List<Product>();
+            while (reader.Read())
+            {
+                Product product = new Product();
+                product.ProductId = (int)reader["ProductId"];
+                product.ProductName = reader["ProductName"].ToString();
+                product.Price =(float) Convert.ToDouble(reader["Price"]);
+                products.Add(product);
+            }
+            return products;
+        }
+
+        public Product getProduct(int id)
+        {
+            string sql = "SELECT * FROM Products WHERE ProductId="+id;
+            SqlDataReader reader = dataAccess.GetData(sql);
+            reader.Read();
+            Product product = new Product();
+            product.ProductName = reader["ProductName"].ToString();
+            product.Price = (float)Convert.ToDouble(reader["Price"]);
+            product.CategoryId = (int)reader["CategoryId"];
+
+            return product;
+        }
+
+        public int Insert(Product product)
+        {
+            string sql = "INSERT INTO Products(ProductName, Price, CategoryId) VALUES ('" + product.ProductName + "', '" + product.Price + "', '" + product.CategoryId + "')";
+            int result = dataAccess.ExecuteQuery(sql);
+            return result;
+        }
+
+        public int Update(Product product)
+        {
+            string sql = "UPDATE Products SET ProductName='" + product.ProductName + "', Price='" + product.Price + "', CategoryId=" + product.CategoryId + " WHERE ProductId=" + product.ProductId + "";
+            int reasult = dataAccess.ExecuteQuery(sql);
+            return reasult;
+        }
+        public int Delete(int id)
+        {
+            string sql = "DELETE FROM Products WHERE ProductId=" + id;
+            int result = dataAccess.ExecuteQuery(sql);
+            return result;
+        }
+    }
+}
