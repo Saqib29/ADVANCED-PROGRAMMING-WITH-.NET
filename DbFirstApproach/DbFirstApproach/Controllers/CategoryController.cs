37460bdc49fafdedba012fcffb00900d32d0cef0
Saqib <saqib29111@gmail.com>
2021-02-24 02:56:24 +0600

    Inventory Management System With Entity Framework in Database First Approach

---
+++ DbFirstApproach/DbFirstApproach/Controllers/CategoryController.cs
@@ -0,0 +1,67 @@
+﻿using DbFirstApproach.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DbFirstApproach.Controllers
+{
+    public class CategoryController : Controller
+    {
+        InventoryDbContext context = new InventoryDbContext();
+        public ActionResult Index()
+        {
+            return View(context.Categories.ToList());
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            context.Categories.Add(category);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var categoryToEdit = context.Categories.Find(id);
+            return View(categoryToEdit);
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, Category category)
+        {
+            category.CategoryId = id;
+            var categoryToEdit = context.Categories.Find(id);
+            categoryToEdit.CategoryName = category.CategoryName;
+            context.SaveChanges();
+
+            //context.Entry(category).State = System.Data.Entity.EntityState.Modified;
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var categoryToDelete = context.Categories.Find(id);
+            return View(categoryToDelete);
+        }
+        [HttpPost, ActionName("Delete")]
+        public ActionResult ConfirmDelete(int id)
+        {
+            context.Categories.Remove(context.Categories.Find(id));
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            var categoryOfDetails = context.Categories.Find(id);
+            return View(categoryOfDetails);
+        }
+    }
+}
---
+++ DbFirstApproach/DbFirstApproach/Controllers/ProductController.cs
@@ -0,0 +1,57 @@
+﻿using DbFirstApproach.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DbFirstApproach.Controllers
+{
+    public class ProductController : Controller
+    {
+        InventoryDbContext context = new InventoryDbContext();
+        public ActionResult Index()
+        {
+            return View(context.Products.ToList());
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ViewBag.categories = context.Categories.ToList();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            context.Products.Add(product);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            ViewBag.categories = context.Categories.ToList();
+            return View(context.Products.Find(id));
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, Product product)
+        {
+            product.ProductId = id;
+            context.Entry(product).State = System.Data.Entity.EntityState.Modified;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            return View(context.Products.Find(id));
+        }
+        [HttpPost, ActionName("Delete")]
+        public ActionResult ConfirmDelete(int id)
+        {
+            context.Products.Remove(context.Products.Find(id));
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
